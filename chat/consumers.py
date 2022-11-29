d2a93ed9482d882d49a01f09e8f806d53980c26b
Justin W <justinw@archlinux.localdomain>
2022-11-29 15:06:03 +0000

    Remove debug statements

--- chat/consumers.py
+++ chat/consumers.py
@@ -39,10 +39,11 @@ class ChatServiceConsumer(AsyncJsonWebsocketConsumer):
 
 
     async def authorise(self, data):
-        try:
+        
             json_data = json.loads(data)
-
+            print(json_data)
             if await self.query(json_data["mail_id"]) == False:
+                
                 await self.send_json({
                     "event": "auth",
                     "status": "error",
@@ -50,13 +51,14 @@ class ChatServiceConsumer(AsyncJsonWebsocketConsumer):
                 })
                 await self.close(3001)
 
-            if json_data["password"] == await self.get_user_password(await self.get_user(json_data["mail_id"])):
+            if json_data["password"] == await self.get_user_password(json_data["mail_id"]):
                 self.is_authorised = True
                 await self.channel_layer.group_add(await self.get_user_id(json_data["mail_id"]), self.channel_name)
                 await self.send_json({
                     "event": "auth",
                     "status": "done",
-                    "client_id": str(await self.get_user_id(json_data["mail_id"]))
+                    "client_id": str(await self.get_user_id(json_data["mail_id"])),
+                    "username": str(await self.get_username(json_data["mail_id"]))
                 })
             else:
                 await self.send_json({
@@ -65,14 +67,14 @@ class ChatServiceConsumer(AsyncJsonWebsocketConsumer):
                     "message": "Incorrect Password"
                 })
                 await self.close(3001)
-        except  Exception as e:
-            print("Retrying, ", e)
+        
+        
 
 
     async def chat_message(self, event):
         try:
             await self.send_json({
-                "event": "message"
+                "event": "message",
                 "message": event["message"],
                 "author": event["author"],
                 "time": event["time"],
@@ -86,8 +88,12 @@ class ChatServiceConsumer(AsyncJsonWebsocketConsumer):
         return ChatUser.objects.filter(mail_id=mail_id).exists()
         
     @sync_to_async
+    def get_username(self, pk):
+        return ChatUser.objects.get(mail_id=pk).username
+
+    @sync_to_async
     def get_user_password(self, pk):
-        return ChatUser.objects.get(pk=pk).password
+        return ChatUser.objects.get(mail_id=pk).password
 
     @sync_to_async
     def get_user(self, mail_id) -> ChatUser:
@@ -111,8 +117,10 @@ class RegisterUserConsumer(AsyncJsonWebsocketConsumer):
         json_data = json.loads(text_data)
 
         if json_data["event"] == "register":
+            
             try:
                 if await self.query(json_data["mail_id"]) == False:
+                    
                     self._username = json_data["username"]
                     self._password = json_data["password"]
                     self._mail_id = json_data["mail_id"]
@@ -120,8 +128,10 @@ class RegisterUserConsumer(AsyncJsonWebsocketConsumer):
                     self._verification_code = code
                     message = f'Hey {json_data["username"]}, thanks for joining C-Hat.\n\n Your verification code is {code}.\n Enter this code in your C-Hat client to start chatting'
                     send_mail(f'Verify your email', message, None, [json_data["mail_id"]], fail_silently=False)
+                    await self.send_json({"event":"verify"})
 
                 else:
+                    print("mail exists")
                     await self.send_json({
                         "event": "info",
                         "message": "Mail ID already exists"
@@ -133,7 +143,7 @@ class RegisterUserConsumer(AsyncJsonWebsocketConsumer):
                 print("Can't register user: ", e)
 
         elif json_data["event"] == "confirmation":
-            print(json_data["code"] == str(self._verification_code))
+            
             if json_data["code"] == str(self._verification_code):
                 new_user = ChatUser(
                     client_id = await self.generate_client_id(),
@@ -151,7 +161,8 @@ class RegisterUserConsumer(AsyncJsonWebsocketConsumer):
             else:
                 await self.send_json({
                     "event": "confirmation",
-                    "status": "error"
+                    "status": "error",
+                    "message": "Incorrect OTP"
                 })
 
         else:
