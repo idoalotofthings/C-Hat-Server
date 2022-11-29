f989595b23a01bce9f89655292e4ec75a3a3c068
Justin W <justinw@archlinux.localdomain>
2022-11-29 16:06:02 +0000

    Modify django runner to load plugins before start

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import json
+import importlib
+from busprovider import bus
+from eventbus.event import Event
 
 def main():
+
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'c_hat_server.settings')
     try:
@@ -15,8 +19,26 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    register_events()
+    load_plugins()
+    
+    bus.start()
+
+    execute_from_command_line(sys.argv)
+    
+
+def register_events():
+    for event in Event:
+        bus.register_event(event)
+
+def load_plugins():
+    try:
+        plugins = json.load(open("plugins.json"))
+        for plugin in plugins:
+            importlib.import_module(f"plugins.{plugin}.{plugin}")
+    except Exception as e:
+        print("Error loading plugins:", e)
 
 if __name__ == '__main__':
     main()
