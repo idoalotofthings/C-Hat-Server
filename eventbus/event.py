efe9a7e79a605b058f9fb69187053077e61e5cd8
Justin W <justinw@archlinux.localdomain>
2022-11-29 16:05:41 +0000

    Add more events

--- eventbus/event.py
+++ eventbus/event.py
@@ -3,3 +3,10 @@ import enum
 class Event(enum.Enum):
     ON_START = 0
     ON_STOP = 1
+    ON_MESSAGE = 2
+    ON_USER_LOGIN = 3
+    ON_USER_REGISTER = 4
+    ON_CLIENT_CONNECT = 5
+    ON_OTP_SENT = 6
+    ON_EMAIL_VERIFY_FAIL = 7
+
